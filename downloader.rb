6a597a04bb56da4d20aecd39fccc3e550cb4a0f1
n07028 <nett-tv-test@nrk.no>
2016-01-06 16:59:02 +0100

    First commit

---
+++ downloader.rb
@@ -0,0 +1,34 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'nokogiri'
+require 'net/http'
+require 'open-uri'
+
+
+class Downloader
+
+  def initialize(cast_url = 'http://podcast.badeog.no/kortapplaus/podcast.xml?1001250')
+    @cast_url = cast_url
+  end
+
+  def download_m3us
+    urls = []
+    @doc = Nokogiri::XML(open("http://podcast.badeog.no/kortapplaus/podcast.xml?1001250"))
+    list = @doc.xpath("//enclosure/@url")
+
+    list.each do|el|
+      urls.push(el)
+    end
+
+    #This works but tryna use multi threads
+    urls.each do|url|
+      system %x{ wget -P ~/audio/ #{url} }
+      break
+    end
+  end
+
+
+end
+
+test = Downloader.new('http://podcast.badeog.no/kortapplaus/podcast.xml?1001250')
+test.download_m3us
